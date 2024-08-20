c93c9c6764849cdd6b981d68f6f1ff1faf51aafc
mahmoud-mhamed <mahmoud1272022@gmail.com>
2024-08-20 23:59:43 +0300

    [FEAT] add log package

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::pattern('id', '[0-9]+');
+Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'index']);
 
 Route::get('/', function () {
     return redirect(route('login'));
